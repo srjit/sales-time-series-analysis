c3c58d09d4a5bc0487a96dce701234af6d3cb77c
Sreejith Sreekumar <sreekumar.s@FACC02TQ1PTFVH4.tld>
2019-02-15 19:50:58 -0500

    initial commit

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -8,7 +8,6 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
---
+++ src/app/app.py
@@ -0,0 +1,15 @@
+#importing libraries
+import os
+
+import flask
+import pickle
+from flask import Flask, render_template, request
+
+#creating instance of the class
+app=Flask(__name__)
+
+#to tell flask what url shoud trigger the function index()
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
